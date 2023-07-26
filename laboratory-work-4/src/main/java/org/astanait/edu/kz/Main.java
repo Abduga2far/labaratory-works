fcb01bc1ec24df461acb931f20788bc000dafa53
Abduga2far <221149@astanait.edu.kz>
2023-07-27 05:46:02 +0600

    lab-work-4 added Vertex class with their methods, and added Main class

---
+++ laboratory-work-4/src/main/java/org/astanait/edu/kz/Main.java
@@ -0,0 +1,11 @@
+package org.astanait.edu.kz;
+
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+
